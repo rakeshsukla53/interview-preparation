f1215cac8a4a698c15f06841ac9e4ae984cc87e9
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-08-30 18:08:10 -0400

    bloom filters

---
+++ Rakesh/bloom-filters/bloom_filters.py
@@ -0,0 +1,42 @@
+__author__ = 'rakesh'
+
+import pyblume
+
+BLOOM_FILTER_MAX_FILESIZE = 1024 * 1024 * 500
+BLOOM_ERROR_RATE = 0.000001
+fileloc = "/tmp/test.blume"
+
+bf = pyblume.Filter(BLOOM_FILTER_MAX_FILESIZE, BLOOM_ERROR_RATE, fileloc)
+
+for i in range(313):
+
+    bf.add(str(i))
+
+bf.close()
+
+check = [str(j) for j in range(300, 400)]
+
+print check
+
+bf = pyblume.open(fileloc, for_write=False)
+for x in check:
+    if x in bf:
+        print "Found"
+bf.close()
+
+'''
+NOTES:
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
