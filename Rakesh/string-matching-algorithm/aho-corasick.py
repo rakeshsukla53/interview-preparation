175e92a16f2aea30b37f7a12856f0fe113515c44
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-08-31 20:38:51 -0400

    aho-corasick-algorithm

---
+++ Rakesh/string-matching-algorithm/aho-corasick.py
@@ -0,0 +1,7 @@
+__author__ = 'rakesh'
+
+#https://www.youtube.com/watch?v=d24CyiU1JFk
+
+#In computer science, the Aho–Corasick string matching algorithm is a string searching algorithm invented by Alfred V. Aho and Margaret J. Corasick. It is a kind of dictionary-matching algorithm that locates elements of a finite set of strings (the "dictionary") within an input text. It matches all patterns simultaneously. The complexity of the algorithm is linear in the length of the patterns plus the length of the searched text plus the number of output matches. Note that because all matches are found, there can be a quadratic number of matches if every substring matches (e.g. dictionary = a, aa, aaa, aaaa and input string is aaaa).
+
+
