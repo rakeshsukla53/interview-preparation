1bb957c4f5f4867fc9919c93e8dae6dbca1c5bb4
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-09-11 19:01:49 -0400

    general programming questions

--- Rakesh/General Interview Questions/CountMultiplicativePairs.py
+++ Rakesh/General Interview Questions/CountMultiplicativePairs.py
@@ -1,2 +1,23 @@
 __author__ = 'rakesh'
 
+from itertools import combinations
+
+
+def C(n, A, B):
+
+    return A[n] + float(B[n]) / 1000000
+
+def solution(A, B):
+
+    count = 0
+
+    for i in combinations(list(range(1, len(A))), 2):
+
+        P, Q = i
+        if C(P, A, B)*C(Q, A, B) >= C(P, A, B) +C(Q, A, B):
+
+            count += 1
+
+    return count
+
+print solution([0, 1, 2, 2, 3, 5], [500000, 500000, 0, 0, 0, 20000])
