b19a6236664b361ef736e43def1b5e69042ce940
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-09-12 00:31:39 -0400

    Cipher breakdown

--- Rakesh/General Interview Questions/cipher.py
+++ Rakesh/General Interview Questions/cipher.py
@@ -1,5 +1,14 @@
 __author__ = 'rakesh'
 
+#Logic is now build, just you have to write the program
+
+#just see how it works and then build the solution
+
+#Solve it in your free time https://www.hackerrank.com/challenges/cipher
+
+
+
+
 
 
 
