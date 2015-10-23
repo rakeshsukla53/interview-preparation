ae8da69a13cb0ab4d6130e8c92eafa27287859bb
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-10-22 20:23:04 -0400

    word_problem_approach

---
+++ Rakesh/word_problem/word_break_problem_no_need_of_permutation.py
@@ -0,0 +1,28 @@
+__author__ = 'rakesh'
+
+# The idea is the following:
+#
+#     d is an array that contains booleans
+#
+#     d[i] is True if there is a word in the dictionary that ends at ith index of s AND d is also True at the beginning of the word
+#
+# Example:
+#
+#     s = "leetcode"
+#
+#     words = ["leet", "code"]
+#
+#     d[3] is True because there is "leet" in the dictionary that ends at 3rd index of "leetcode"
+#
+#     d[7] is True because there is "code" in the dictionary that ends at the 7th index of "leetcode" AND d[3] is True
+#
+# The result is the last index of d.
+
+def word_break(s, words):
+    d = [False] * len(s)
+    for i in range(len(s)):
+        for w in words:
+            if w == s[i-len(w)+1:i+1] and (d[i-len(w)] or i-len(w) == -1):
+                d[i] = True
+    return d[-1]
+
