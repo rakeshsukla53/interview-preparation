540f92c8a66fea5540ba969e308be4635d4f7d5f
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-11-16 23:58:14 -0500

    generator basics cleared

--- Rakesh/python-basics/generator and yield for massive performance.md
+++ Rakesh/python-basics/generator and yield for massive performance.md
@@ -7,6 +7,17 @@ generate the stuff which I need and just throw it away latter on. It improves pe
 
 yield is like `return` in generator
 
+# generator and how to use it
+
+generators are used to generate a series of values
+yield is like the return of generator functions
+The only other thing yield does is save the "state" of a generator function
+A generator is just a special type of iterator
+Like iterators, we can get the next value from a generator using next()
+    for gets values by calling next() implicitly
+
+
+
 
 
 
---
+++ Rakesh/python-generator/what is python_generator.py
@@ -0,0 +1,29 @@
+
+
+def generator():
+
+    for i in xrange(10000000):
+        yield i  # this is basically just like a return object for generator
+
+        # since we are not storing anything in memory we don't take to store anything in a list
+
+        # there is property of yield through which we can store previous values and remembers it
+
+print generator()  # it doesn't store anything in memory if you don't want to use it
+
+# it will return a generator object
+
+def without_generator():
+    result = [] # now you need to store value here
+
+    for j in range(1000):
+        result.append(j)
+
+    return result
+
+print without_generator()  # this will return the whole list
+
+
+
+
+
