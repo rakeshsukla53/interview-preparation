9e069decd093c9092c1bf36b2c1e53f54952539d
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-12-21 02:10:19 -0500

    edit distance is not levenstein

--- Rakesh/sorting/char_digit_sorting.py
+++ Rakesh/sorting/char_digit_sorting.py
@@ -1,15 +1,16 @@
-
-
-a = ['c1', 'c2', 'a3', 'a1', 'a2', 'b3', 'b1', 'b2']
-
-a.sort(cmp=lambda x, y: cmp(y, x))
-
-print a
-
-# >>> a = ['c1', 'b2', 'a3', 'a1', 'a2', 'b3', 'b1', 'c2']
-# >>>
-# >>> a.sort(cmp=lambda x, y: cmp(y + x, x + y))
-# >>>
-# >>> a[::-1]
-# ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2']
+# a = ['c1', 'c2', 'a3', 'a1', 'a2', 'b3', 'b1', 'b2']
 #
+# a.sort(cmp=lambda x, y: cmp(y, x))
+#
+# print a
+#
+# # >>> a = ['c1', 'b2', 'a3', 'a1', 'a2', 'b3', 'b1', 'c2']
+# # >>>
+# # >>> a.sort(cmp=lambda x, y: cmp(y + x, x + y))
+# # >>>
+# # >>> a[::-1]
+# # ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2']
+# #
+
+
+
