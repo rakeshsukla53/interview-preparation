5009bfc3507c3bd9e5a4501c031406d17f19166c
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-01-02 23:21:57 -0500

    two pointer method for 3SUM, 4SUM, 3SUM Smaller problems

--- Rakesh/2 Pointers/3Sum Smaller.py
+++ Rakesh/2 Pointers/3Sum Smaller.py
@@ -3,18 +3,18 @@
 # this question is also known as 3SUM Algorithm
 
 class Solution(object):
-    def threeSumSmaller(self, nums, target):
+    def threeSum(self, nums):
         """
         :type nums: List[int]
-        :type target: int
-        :rtype: int
+        :rtype: List[List[int]]
         """
+
         if len(nums) < 2:
-            return 0
+            return []
 
         nums.sort()
         n = len(nums)
-        count = 0
+        result = set()
         for i in range(n - 2):
             a = nums[i]
             # this is the concept of two pointers. One will start from i + 1, and other from n - 1
@@ -27,18 +27,20 @@ class Solution(object):
                 b = nums[start]
                 c = nums[end]
                 # if the sum is less than target, then capture the value
-                if a + b + c < target:
-                    count += end - start
+                if a + b + c < 0:
                     start += 1
-                else:
+                elif a + b + c > 0:
                     end -= 1
-
+                else:
+                    result.add((a, b, c))
+                    start += 1
+                    end -= 1
                     # if you need to decrease the value of end here anyway since you're checking for all the values less than target.
 
         # return the final count or you can just yield it
-        return count
+        return map(list, list(result))
 
-print Solution().threeSumSmaller([3, 1, 0, -2], 4)
+print Solution().threeSum([-2, 0, 1, 1, 2])
 
 # if you want to check the value of zero then you need can insert all the values into a hash table
 
