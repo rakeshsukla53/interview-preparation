264decd5854805260374b7b36164ddffc518e6a8
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-01-06 00:44:27 -0500

    group by in action

--- Rakesh/General Interview Questions/group by in python.py
+++ Rakesh/General Interview Questions/group by in python.py
@@ -1,24 +1,29 @@
 
-# from itertools import groupby
-#
-# data = [2, 1, 2, 2, 2]
-#
-# for k, g in groupby(data, lambda x: x == 2):
-#     if k:
-#         print list(g)
+from itertools import groupby
+
+data = [2, 1, 2, 2, 2]
+
+for k, g in groupby(data, lambda x: x == 2):
+    if k:
+        print list(g)
 
 from itertools import groupby
 
-data = 'AAABBBCCCDDDDDDEEEEFEEEEEFEEEE'
+data = 'AAABBBCCCDDDDDDEEEEEEEEEEEEE'
 
 max_length, character = 0, ''
 
 for k, g in groupby(data):
     if k:
         group = list(g)
-        print group
         if len(group) > max_length:
             max_length = len(group)
-            character = group[0]
+            character =
 print max_length, character
+
+from collections import Counter
+from itertools import groupby
+
+data = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
 
