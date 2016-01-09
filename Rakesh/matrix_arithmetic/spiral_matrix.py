9c119ba8566de8f24ade937c709843d910c5bb12
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-01-08 19:56:07 -0500

    really bad solution for find spiral of the matrix

---
+++ Rakesh/matrix_arithmetic/spiral_matrix.py
@@ -0,0 +1,65 @@
+
+from collections import deque
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        nrows = deque()
+        ncols = deque()
+        nrows.extend(range(len(matrix)))
+        ncols.extend(range(len(matrix[0])))
+        result = []
+
+        while True:
+
+            if not ncols or not nrows:
+                break
+
+            row = ncols[0]
+            for i in ncols:  # go over the row
+                result.append(matrix[row][i])
+            nrows.popleft()  # since row is traversed, remove it
+
+            if not ncols or not nrows:
+                break
+
+            col = ncols[-1]
+            for j in nrows:
+                result.append(matrix[j][col])
+            ncols.pop()  # the entire col has been traversed
+
+            if not ncols or not nrows:
+                break
+
+            row = nrows[-1]
+            ncols.reverse()
+            for k in ncols:  # now moving backwards
+                result.append(matrix[row][k])
+            nrows.pop()  # the entire last row is traversed
+            ncols.reverse()
+
+            if not ncols or not nrows:
+                break
+
+            col = ncols[0]
+            nrows.reverse()
+            for p in nrows:
+                result.append(matrix[p][col])
+            ncols.popleft()
+            nrows.reverse()
+
+            if not ncols or not nrows:
+                break
+
+        return result
+
+matrix = [
+ [1, 2, 3],
+ [4, 5, 6],
+ [7, 8, 9],
+]
+
+print Solution().spiralOrder(matrix)
---
+++ Rakesh/python-functionals/lambda_examples.py
@@ -0,0 +1,6 @@
+
+
+
+[map(lambda i: 1 if i > '4' else 0, y) for y in x]
+
+
