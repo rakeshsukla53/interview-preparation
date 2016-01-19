b6f0df99d5db327f31fe3f9afda018f710af86a7
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-01-19 12:42:54 -0500

    updating state variables in python

---
+++ Rakesh/python-basics/deque in python incredibly important.py
@@ -0,0 +1,13 @@
+
+names = ['raymond', 'rachel', 'mathew', 'roger', 'betty', 'melissa', 'judith']
+
+del names[0]
+names.pop(0)
+names.insert(0, 'mark')
+
+# this is absolutely wrong
+# you should be using deque because it has popleft and appendright
+
+# code efficiency goes super high
+
+
