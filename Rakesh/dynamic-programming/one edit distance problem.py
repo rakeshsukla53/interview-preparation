3f15a8fdcd91a5a2fc6202fb1eec008cf86588ff
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-01-21 02:04:11 -0500

    one edit distance summarized

--- Rakesh/dynamic-programming/one edit distance problem.py
+++ Rakesh/dynamic-programming/one edit distance problem.py
@@ -12,24 +12,21 @@ class Solution(object):
         if abs(len_1 - len_2) > 1 or (len(list(set(s) - set(t))) > 1 or len(list(set(t) - set(s))) > 1) or (not s and not t):
             return False
 
-        if abs(len_1 - len_2) == 1:
-            x = [[0]*(len_2+1) for _ in range(len_1+1)]   # the matrix whose last element -> edit distance
-            for i in range(0, len_1+1):  # initialization of base case values
-                x[i][0] = i
-            for j in range(0, len_2+1):
-                x[0][j] = j
-            for i in range(1, len_1+1):
-                for j in range(1, len_2+1):
-                    if s[i-1] == t[j-1]:
-                        x[i][j] = x[i-1][j-1]
-                        if x[i][j] > 1:  # for one edit distance problem this value should remain one till the end
-                            return False
-                    else:
-                        x[i][j] = min(x[i][j-1], x[i-1][j], x[i-1][j-1])+1
+        x = [[0]*(len_2+1) for _ in range(len_1+1)]   # the matrix whose last element -> edit distance
+        for i in range(0, len_1+1):  # initialization of base case values
+            x[i][0] = i
+        for j in range(0, len_2+1):
+            x[0][j] = j
+        for i in range(1, len_1+1):
+            for j in range(1, len_2+1):
+                if s[i-1] == t[j-1]:
+                    x[i][j] = x[i-1][j-1]
+                    if x[i][j] > 1:  # for one edit distance problem this value should remain one till the end
+                        return False
+                else:
+                    x[i][j] = min(x[i][j-1], x[i-1][j], x[i-1][j-1]) + 1
 
-            return True if x[i][j] == 1 else False
-        else:
-            return False
+        return True
 
 print Solution().isOneEditDistance('abbd', 'abd')
 
