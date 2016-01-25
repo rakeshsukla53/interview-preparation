2938a360e2eca62278e35f1a57a15b37b88e1a95
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-01-24 19:56:02 -0500

    isomorphic algorithm

---
+++ Rakesh/string-matching-algorithm -general-strings/isomorphic strings.py
@@ -0,0 +1,22 @@
+
+from collections import defaultdict
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        ismorphic_string = defaultdict()
+        store_mapping = defaultdict()
+        for letter1, letter2 in zip(s, t):
+            if letter1 in ismorphic_string and not ismorphic_string[letter1] == letter2:
+                return False
+            elif letter1 not in ismorphic_string and letter2 in store_mapping:
+                return False
+            ismorphic_string[letter1] = letter2
+            store_mapping[letter2] = ''
+        return True
+
+print Solution().isIsomorphic('foo', 'bar')
