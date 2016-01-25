f86dabb355e47e73b3c3ab3fd6a33f6e19bdb787
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-01-25 01:10:10 -0500

    partition problem can be solved by quick sort

--- Rakesh/matrix_arithmetic/matrix_rotation_reverse_awesomness.py
+++ Rakesh/matrix_arithmetic/matrix_rotation_reverse_awesomness.py
@@ -1,11 +1,10 @@
-# A = [
-#  [1, 2, 3],
-#  [8, 9, 4],
-#  [7, 6, 5]
-# ]
-#
-# print A[::-1]  # reverses the whole matrix
-#
-# print zip(*A[::-1])  # rotates the whole matrix clockwise 90 degree
+A = [
+ [1, 2, 3],
+ [8, 9, 4],
+ [7, 6, 5]
+]
 
+# print A[::-1]  # reverses the whole matrix
+
+matrix = zip(*A[::-1])  # rotates the whole matrix clockwise 90 degree
 
---
+++ Rakesh/quick-sorting/Partition List think about quick sort.py
@@ -0,0 +1,22 @@
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def partition(self, head, x):
+        """
+        Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+For example,
+Given 1->4->3->2->5->2 and x = 3,
+return 1->2->2->4->3->5.
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+
