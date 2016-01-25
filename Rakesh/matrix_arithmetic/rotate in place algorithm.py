a12e0c1c9544d164ed8fae40f1b2c070c03d2166
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-01-25 13:54:40 -0500

    matrix rotation in place by 90 degree

---
+++ Rakesh/matrix_arithmetic/rotate in place algorithm.py
@@ -0,0 +1,18 @@
+
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        matrix[::] = zip(*matrix[::-1])
+
+class Solution:
+    def rotate(self, A):
+        n = len(A)
+        for i in range(n/2):
+            for j in range(n-n/2):
+                A[i][j], A[~j][i], A[~i][~j], A[j][~i] = \
+                         A[~j][i], A[~i][~j], A[j][~i], A[i][j]
+
+# best solution you can think of
