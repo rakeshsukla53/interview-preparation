5bf4f680217dc06290be861f60f02b510706ef0a
rakeshsukla53 <rakesh.sukla53@gmail.com>
2016-01-31 01:45:47 -0500

    string replacement with dictionary

--- Rakesh/string-matching-algorithm -general-strings/string replacement with dictionary.py
+++ Rakesh/string-matching-algorithm -general-strings/string replacement with dictionary.py
@@ -9,3 +9,17 @@ result = pattern.sub(lambda x: d[x.group()], s)
 
 print result
 
+#
+# modelName = ['Toyota', 'Nissan',  'Honda']
+# fuelEfficiency = [15.0, 20.0, 30.0]
+# priceGas = 2.41
+# totalCost1 = (10000 * priceGas)/15.0
+# totalCost2 = (10000 * priceGas)/20.0
+# totalCost3 = (10000 * priceGas)/30.0
+# totalcost = [totalCost1, totalCost2, totalCost3]
+#
+# print ("Model", "Cost(MPG)", "TOTALCOST")
+# for i in zip(modelName, fuelEfficiency, totalcost):
+#     print (" ".join(map(str, list(i))))
+
+
